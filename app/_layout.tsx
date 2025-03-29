fca351c49497fbfbd8bc3ccee4c4f42f965cf065
KiranMahn <68765104+KiranMahn@users.noreply.github.com>
2025-03-29 10:17:37 +0000

    update calendar to take to correct day in track

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './(tabs)/home';
 import Calendar from './(tabs)/calendar';
 import Track from './(tabs)/track';
+import { RouteProp } from '@react-navigation/native';
 import Analysis from './(tabs)/analysis';
 import Profile from './(tabs)/profile';
 import Learn from './(tabs)/learn';
 import Settings from './(tabs)/settings';
-import { Button } from '@react-navigation/elements';
 import { TouchableOpacity } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
@@ -17,148 +17,138 @@ const Stack = createStackNavigator();
 type RootStackParamList = {
   Home: undefined;
   Calendar: undefined;
-  Track: undefined;
+  Track: { date?: string }; // Add date as an optional parameter
   Analysis: undefined;
   Learn: undefined;
   Settings: undefined;
 };
 
 const App = () => {
-    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
-    return (
-      <SettingsProvider>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen 
-            name="Home" 
-            component={Home} 
-            options={{
-              title: 'Home',
-              headerStyle: {
-                backgroundColor: '#rgb(0, 150, 136);',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 20,
-
-              },
-              headerTitleAlign: 'center',
-              headerRight: () => (
-                <TouchableOpacity onPress={() => navigation.navigate('Settings')}
-                style={{ marginRight: 15 }}>
-                  <Ionicons name="settings" size={21} color="white" />
-                </TouchableOpacity>
-              ),
-            }}
-          />
-          <Stack.Screen 
-              name="Calendar" 
-              component={Calendar} 
-              options={{
-                title: 'Calendar',
-                headerStyle: {
-                  backgroundColor: '#rgb(0, 150, 136);',
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                  fontWeight: 'bold',
-                  fontSize: 20,
-                },
-                headerTitleAlign: 'center',
-                headerRight: () => (
-                  <TouchableOpacity onPress={() => navigation.navigate('Settings')}
-                  style={{ marginRight: 15 }}>
-                    <Ionicons name="settings" size={21} color="white" />
-                  </TouchableOpacity>
-                ),
-              }}
-            />
-            <Stack.Screen 
-            name="Learn" 
-            component={Learn} 
-            options={{
-              title: 'Learn',
-              headerStyle: {
-                backgroundColor: '#rgb(0, 150, 136);',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 20,
-
-              },
-              headerTitleAlign: 'center',
-              headerRight: () => (
-                <TouchableOpacity onPress={() => navigation.navigate('Settings')}
-                style={{ marginRight: 15 }}>
-                  <Ionicons name="settings" size={21} color="white" />
-                </TouchableOpacity>
-              ),
-            }}
-          />
-          <Stack.Screen 
-            name="Track" 
-            component={Track} 
-            options={{
-              title: 'Track',
-              headerStyle: {
-                backgroundColor: '#rgb(0, 150, 136);',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 20,
-
-              },
-              headerTitleAlign: 'center',
-            }}
-          />
-          <Stack.Screen 
-            name="Analysis" 
-            component={Analysis} 
-            options={{
-              title: 'Analysis',
-              headerStyle: {
-                backgroundColor: '#rgb(0, 150, 136);',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 20,
-
-              },
-              headerTitleAlign: 'center',
-              headerRight: () => (
-                <TouchableOpacity onPress={() => navigation.navigate('Settings')}
-                style={{ marginRight: 15 }}>
-                  <Ionicons name="settings" size={21} color="white" />
-                </TouchableOpacity>
-              ),
-            }}
-          />
-          <Stack.Screen 
-            name="Settings" 
-            component={Settings} 
-            options={{
-              title: 'Settings',
-              headerStyle: {
-                backgroundColor: '#rgb(0, 150, 136);',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 20,
-
-              },
-              headerTitleAlign: 'center',
-            }}
-          />
-        </Stack.Navigator>  
-      </SettingsProvider>
-
-    );
-    
+  return (
+    <SettingsProvider>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            title: 'Home',
+            headerStyle: {
+              backgroundColor: '#rgb(0, 150, 136);',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+            headerTitleAlign: 'center',
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={{ marginRight: 15 }}>
+                <Ionicons name="settings" size={21} color="white" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Stack.Screen 
+          name="Calendar" 
+          component={Calendar} 
+          options={{
+            title: 'Calendar',
+            headerStyle: {
+              backgroundColor: '#rgb(0, 150, 136);',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+            headerTitleAlign: 'center',
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={{ marginRight: 15 }}>
+                <Ionicons name="settings" size={21} color="white" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Stack.Screen 
+          name="Learn" 
+          component={Learn} 
+          options={{
+            title: 'Learn',
+            headerStyle: {
+              backgroundColor: '#rgb(0, 150, 136);',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+            headerTitleAlign: 'center',
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={{ marginRight: 15 }}>
+                <Ionicons name="settings" size={21} color="white" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Stack.Screen 
+          name="Track" 
+          component={Track} 
+          options={{
+            title: 'Track',
+            headerStyle: {
+              backgroundColor: '#rgb(0, 150, 136);',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+            headerTitleAlign: 'center',
+          }}
+          initialParams={{ date: new Date().toISOString() }} // Pass the current date as the default prop
+        />
+        <Stack.Screen 
+          name="Analysis" 
+          component={Analysis} 
+          options={{
+            title: 'Analysis',
+            headerStyle: {
+              backgroundColor: '#rgb(0, 150, 136);',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+            headerTitleAlign: 'center',
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={{ marginRight: 15 }}>
+                <Ionicons name="settings" size={21} color="white" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Stack.Screen 
+          name="Settings" 
+          component={Settings} 
+          options={{
+            title: 'Settings',
+            headerStyle: {
+              backgroundColor: '#rgb(0, 150, 136);',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+            headerTitleAlign: 'center',
+          }}
+        />
+      </Stack.Navigator>  
+    </SettingsProvider>
+  );
 };
 
 export default App;
