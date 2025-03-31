837fd623c156fe7a74be970c2beff0d70887548d
KiranMahn <68765104+KiranMahn@users.noreply.github.com>
2025-03-31 18:43:59 +0100

    add test for learn

---
+++ components/__tests__/Learn-test.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import Learn from '../../app/(tabs)/learn';
+import { SettingsContext } from '../../app/settings-context';
+import articles from '../../articles.json';
+import { Linking } from 'react-native';
+
+jest.mock('react-native/Libraries/Linking/Linking', () => ({
+  openURL: jest.fn(),
+}));
+jest.mock('@react-navigation/native', () => ({
+    useNavigation: jest.fn(() => ({ navigate: jest.fn() })),
+}));
+type Article = {
+    Title: string;
+    Author: string;
+    Date: string;
+    URL: string;
+    keywords: string[];
+};
+
+describe('Learn Component', () => {
+  const mockSettings = {
+    highContrast: false,
+    largeText: false,
+    largeIcons: false,
+  };
+
+  const renderComponent = (settings = mockSettings) => {
+    return render(
+      <SettingsContext.Provider value={{ settings, saveSettings: jest.fn() }}>
+        <Learn />
+      </SettingsContext.Provider>
+    );
+  };
+
+  test('renders correctly', () => {
+    const { getByText } = renderComponent();
+    expect(getByText('For You')).toBeTruthy();
+    expect(getByText('Recent')).toBeTruthy();
+    expect(getByText('Symptom Relief')).toBeTruthy();
+    expect(getByText('Menopause Stages')).toBeTruthy();
+  });
+
+  test('switches tabs when a tab is pressed', () => {
+    const { getByText } = renderComponent();
+    fireEvent.press(getByText('Recent'));
+    expect(getByText('Recent').props.style.fontWeight).toBe('bolder');
+  });
+
+  test('displays articles based on selected tab', () => {
+    const { getByText } = renderComponent();
+    fireEvent.press(getByText('Symptom Relief'));
+    const filteredArticles = (Object.values(articles) as Article[]).filter(article => article.keywords.includes('symptoms'));
+    expect(getByText(filteredArticles[0].Title)).toBeTruthy();
+  });
+
+  test('opens article URL when an article is pressed', () => {
+    const { getByText } = renderComponent();
+    const firstArticle = (Object.values(articles) as Article[])[0];
+    fireEvent.press(getByText(firstArticle.Title));
+    expect(Linking.openURL).toHaveBeenCalledWith(firstArticle.URL);
+  });
+});
